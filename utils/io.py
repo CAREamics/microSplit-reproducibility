69d4c027e3d56aa668a1113119bc46b49ac76036
federico-carrara <federico1.carrara@mail.polimi.it>
2024-10-30 15:33:39 +0100

    moved functions to save configs in utils/

--- utils/io.py
+++ utils/io.py
@@ -1,7 +1,17 @@
 from datetime import datetime
 import os
 from pathlib import Path
-from typing import Union
+from typing import Literal, Optional, Sequence, Union, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from pytorch_lightning.loggers import WandbLogger
+    from careamics.config import (
+        VAEAlgorithmConfig,
+        DataConfig,
+        LVAELossConfig,
+        TrainingConfig,
+    )
+    Config = Union[VAEAlgorithmConfig, DataConfig, LVAELossConfig, TrainingConfig]
 
 
 def get_new_model_version(model_dir: Union[Path, str]) -> int:
@@ -37,7 +47,7 @@ def get_workdir(
 ) -> tuple[Path, Path]:
     """Get the workdir for the current model.
 
-    It has the following structure: "root_dir/YYMM/model_name/version"
+    Workdir path has the following structure: "root_dir/YYMM/model_name/version".
     
     Parameters
     ----------
@@ -68,3 +78,61 @@ def get_workdir(
     except FileExistsError:
         print(f"Workdir {cur_workdir} already exists.")
     return cur_workdir, rel_path
+
+
+def save_config(
+    config: Config, 
+    name: Literal["algorithm", "training", "data", "loss"],
+    save_dir: Union[Path, str],
+    logger: Optional[WandbLogger] = None
+) -> None:
+    """Save the `pydantic` configuration in a JSON file.
+    
+    Parameters
+    ----------
+    config : Config
+        The configuration to save.
+    name : str
+        The name of the configuration.
+    save_dir : Union[Path, str]
+        The directory where the configuration file is saved.
+    logger : Optional[WandbLogger], optional
+        The logger to save the configuration in WANDB, by default None.
+        
+    Returns
+    -------
+    None
+    """
+    with open(os.path.join(save_dir, f"{name}_config.json"), "w") as f:
+        f.write(config.model_dump_json(indent=4))
+
+    if logger:
+        logger.experiment.config.update({f"{name}": config.model_dump()})
+
+
+def save_configs(
+    configs: Sequence[Config, Config, Config, Config],
+    names: Sequence[Literal["algorithm", "training", "data", "loss"]],
+    save_dir: Union[Path, str],
+    logger: Optional[WandbLogger] = None
+) -> None:
+    """Save the `pydantic` configurations in JSON files.
+    
+    Parameters
+    ----------
+    configs : Sequence[Config, Config, Config, Config]
+        The configurations to save.
+    names : Sequence[Literal["algorithm", "training", "data", "loss"]]
+        The names of the configurations.
+    save_dir : Union[Path, str]
+        The directory where the configuration files are saved.
+    logger : Optional[WandbLogger], optional
+        The logger to save the configurations in WANDB, by default None.
+        
+    Returns
+    -------
+    None
+    """
+    for config, name in zip(configs, names):
+        save_config(config, name, save_dir, logger)
+    
