7695216eeb7f9d6e2630582d09fffaa2b8081fb7
federico-carrara <federico1.carrara@mail.polimi.it>
2024-11-07 17:37:22 +0100

    wip: need to fix synthetic eval data loading

--- datasets/ht_iba1_ki64_2023.py
+++ datasets/ht_iba1_ki64_2023.py
@@ -82,17 +82,22 @@ def get_train_val_data(
         print(train_idx)
         fnames = [fnames[i] for i in train_idx]
         fpaths = [os.path.join(datadir, subdir, x) for x in fnames]
+        # print(f"Files dir: {os.path.join(datadir, subdir)}")
+        # print(f"File names: {fnames}")
         data = load_czi(fpaths)
     elif datasplit_type == DataSplitType.Val:
         print(val_idx)
         fnames = [fnames[i] for i in val_idx]
         fpaths = [os.path.join(datadir, subdir, x) for x in fnames]
+        # print(f"Files dir: {os.path.join(datadir, subdir)}")
+        # print(f"File names: {fnames}")
         data = load_czi(fpaths)
     elif datasplit_type == DataSplitType.Test:
         print(test_idx)
         fnames_iba1 = [fnames[i] for i in test_idx]
         fpaths_iba1 = [os.path.join(datadir, subdir, x) for x in fnames_iba1]
-
+        # print(f"Iba1 Files dir: {os.path.join(datadir, subdir)}")
+        # print(f"Iba1 File names: {fnames_iba1}")
         # it contains iba1/iba1ki67 and DAPI.
         data = load_czi(fpaths_iba1)
 
@@ -103,15 +108,17 @@ def get_train_val_data(
             SubDsetType.OnlyIba1P70,
         ]:
             datadir_nuc = os.path.join(
-                datadir, SubDsetType.OnlyIba1.value, f"synthetic_test/{dset_subtype}"
+                datadir, SubDsetType.OnlyIba1.value, f"synthetic_test/{dset_subtype.value}"
             )
-            print("Loading nucleus from", datadir_nuc)
             fnames_nuc = sorted(os.listdir(datadir_nuc))
             fpaths_nuc = [os.path.join(datadir_nuc, x) for x in fnames_nuc]
             fpaths_nuc = [fpath for fpath in fpaths_nuc if not os.path.isdir(fpath)]
+            # print("Nuc File dir:", datadir_nuc)
+            # print(f"Nuc File names: {[os.path.basename(x) for x in fpaths_nuc]}")
             data_nuc = np.concatenate(
                 [load_tiff(fpath_)[None] for fpath_ in fpaths_nuc], axis=0
             )[..., None]
+            # TODO: fix this
             data = np.tile(data[..., 1:], (len(data_nuc), 1, 1, 1))
             data = np.concatenate([data_nuc, data], axis=3)
 
@@ -130,3 +137,17 @@ def get_train_val_data(
         data = data[..., 1:]
         data = np.tile(data, (1, 1, 1, 2))
     return data
+
+
+# if __name__ == '__main__':
+#     from ml_collections.config_dict import ConfigDict
+#     data_config = ConfigDict()
+#     data_config.subdset_type = SubDsetType.OnlyIba1P30
+#     datadir = '/group/jug/ashesh/data/Stefania/20230327_Ki67_and_Iba1_trainingdata/'
+#     data = get_train_val_data(
+#         data_config=data_config, 
+#         datadir=datadir,
+#         datasplit_type=DataSplitType.Test, 
+#         val_fraction=0.1, 
+#         test_fraction=0.8
+#     )
