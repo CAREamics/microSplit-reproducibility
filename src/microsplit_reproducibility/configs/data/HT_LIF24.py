b5dafef16299c71e8903eb279234db004c6cc012
federico-carrara <federico.carrara@fht.org>
2024-12-13 12:53:44 +0100

    added parameters for HT_LIF24

--- src/microsplit_reproducibility/configs/data/HT_LIF24.py
+++ src/microsplit_reproducibility/configs/data/HT_LIF24.py
@@ -15,61 +15,45 @@ class NikolaDataConfig(DatasetConfig):
     # TODO: add description
 
 
-DEFAULT_CONFIG = NikolaDataConfig(
-    data_type=DataType.NicolaData,
-    dset_type="2ms",
-    datasplit_type=DataSplitType.Train,
-    image_size=[64, 64],
-    grid_size=32,
-    channel_idx_list=CH_IDX_LIST,
-    num_channels=len(CH_IDX_LIST),
-    input_idx=len(CH_IDX_LIST) - 1,
-    target_idx_list=list(range(len(CH_IDX_LIST) - 1)),
-    multiscale_lowres_count=3,
-    poisson_noise_factor=-1,
-    enable_gaussian_noise=False,
-    synthetic_gaussian_scale=100,
-    input_has_dependant_noise=True,
-    use_one_mu_std=True,
-    train_aug_rotate=True,
-    target_separate_normalization=True,
-    input_is_sum=False,
-    padding_kwargs={"mode": "reflect"},
-    overlapping_padding_kwargs={"mode": "reflect"},
-)
-
-
-def get_train_data_configs(
+def get_data_configs(
     dset_type: Literal["high", "mid", "low", "verylow", "2ms", "3ms", "5ms", "20ms", "500ms"],
     channel_idx_list: list[Literal[1, 2, 3, 17]],
-) -> tuple[NikolaDataConfig, NikolaDataConfig]:
-    """Get the data configurations to use at training time."""
-    train_data_config = DEFAULT_CONFIG.model_copy(
-        update=dict(
-            dset_type=dset_type,
-            channel_idx_list=channel_idx_list,
-        )  
-    )
-    val_data_config = train_data_config.model_copy(
-        update=dict(
-            datasplit_type=DataSplitType.Val,
-            allow_generation=False,  # No generation during validation
-            enable_random_cropping=False,  # No random cropping on validation.
-        )
-    )
-    return train_data_config, val_data_config
+) -> tuple[NikolaDataConfig, NikolaDataConfig, NikolaDataConfig]:
+    """Get the data configurations to use at training time.
     
+    Parameters
+    ----------
+    dset_type : Literal["high", "mid", "low", "verylow", "2ms", "3ms", "5ms", "20ms", "500ms"]
+        The dataset type to use.
+    channel_idx_list : list[Literal[1, 2, 3, 17]]
+        The channel indices to use.
     
-def get_eval_data_configs(
-    dset_type: Literal["high", "mid", "low", "verylow", "2ms", "3ms", "5ms", "20ms", "500ms"],
-    channel_idx_list: list[Literal[1, 2, 3, 17]],
-) -> tuple[NikolaDataConfig, NikolaDataConfig]:
-    """Get the data configurations to use at training time."""
-    train_data_config = DEFAULT_CONFIG.model_copy(
-        update=dict(
-            dset_type=dset_type,
-            channel_idx_list=channel_idx_list,
-        )  
+    Returns
+    -------
+    tuple[NikolaDataConfig, NikolaDataConfig]
+        The train, validation and test data configurations.
+    """
+    train_data_config = NikolaDataConfig(
+        data_type=DataType.NicolaData,
+        dset_type=dset_type,
+        datasplit_type=DataSplitType.Train,
+        image_size=[64, 64],
+        grid_size=32,
+        channel_idx_list=channel_idx_list,
+        num_channels=len(channel_idx_list),
+        input_idx=len(channel_idx_list) - 1,
+        target_idx_list=list(range(len(channel_idx_list) - 1)),
+        multiscale_lowres_count=3,
+        poisson_noise_factor=-1,
+        enable_gaussian_noise=False,
+        synthetic_gaussian_scale=100,
+        input_has_dependant_noise=True,
+        use_one_mu_std=True,
+        train_aug_rotate=True,
+        target_separate_normalization=True,
+        input_is_sum=False,
+        padding_kwargs={"mode": "reflect"},
+        overlapping_padding_kwargs={"mode": "reflect"},
     )
     val_data_config = train_data_config.model_copy(
         update=dict(
---
+++ src/microsplit_reproducibility/configs/parameters/HT_LIF24.py
@@ -0,0 +1,82 @@
+import os
+from typing import Literal
+
+from ._base import SplittingParameters
+
+NOISE_MODEL_ROOT_PATH = "/group/jug/ashesh/training/noise_model/2406/"
+NM_2_DIR_NUM = {
+    "2ms": {
+        1: "10",
+        2: "11",
+        3: "1",
+    },
+    "3ms": {
+        1: "3",
+        2: "4",
+        3: "5",
+    },
+    "5ms": {
+        0: "6",
+        1: "7",
+        1: "8",
+        3: "9",
+    },
+    "20ms": {
+        1: "13",
+        2: "14",
+        3: "15",
+    },
+    "500ms": {
+        1: "17",
+        2: "18",
+        3: "19",
+    },
+}
+
+def _get_nm_paths(
+    dset_type: Literal["2ms", "3ms", "5ms", "20ms", "500ms"],
+    channel_idx_list: list[Literal[1, 2, 3, 17]],    
+) -> list[str]:
+    nm_paths = []
+    for channel_idx in channel_idx_list:
+        if channel_idx == 17:
+            channel_idx = 0
+        fname = f"GMMNoiseModel_nikola_denoising_input-uSplit_20240531_{dset_type}SNR_channel{channel_idx}__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz"
+        nm_paths.append(os.path.join(NOISE_MODEL_ROOT_PATH, NM_2_DIR_NUM[dset_type][channel_idx], fname))
+    return nm_paths
+
+
+def get_musplit_parameters(
+    channel_idx_list: list[int],
+) -> dict:
+    return SplittingParameters(
+        algorithm="musplit",
+        img_size=(64, 64),
+        target_channels=len(channel_idx_list),
+        multiscale_count=3,
+        predict_logvar="pixelwise",
+        loss_type="musplit",
+        kl_type="kl_restricted",
+    ).model_dump()
+
+
+def get_microsplit_parameters(
+    dset_type: Literal["2ms", "3ms", "5ms", "20ms", "500ms"],
+    channel_idx_list: Literal[1, 2, 3, 17],
+) -> dict:
+    nm_paths = _get_nm_paths(dset_type, channel_idx_list)
+    return SplittingParameters(
+        algorithm="denoisplit",
+        img_size=(64, 64),
+        target_channels=len(channel_idx_list),
+        multiscale_count=3,
+        predict_logvar="pixelwise",
+        loss_type="denoisplit_musplit",
+        nm_paths=nm_paths,
+        kl_type="kl_restricted",
+    ).model_dump()
+
+
+def get_eval_params() -> dict:
+    raise NotImplementedError("Evaluation parameters not implemented for HT_LIF24.")
+    return SplittingParameters().model_dump()
