22704acf063df8940df0895d6df84c11ebcabdb7
CatEek <zubarev.ia@gmail.com>
2025-02-04 11:59:13 +0100

    repo cleanup

--- run/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ['evaluate', 'train_muSplit', 'train_denoiSplit']
-
-from .evaluate import evaluate
-from .train_muSplit import train_muSplit
-from .train_denoiSplit import train_denoiSplit
