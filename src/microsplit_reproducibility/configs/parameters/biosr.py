85aa5533fcbcafcbf320200cfe06a36ca1dcf164
CatEek <zubarev.ia@gmail.com>
2025-02-05 16:09:41 +0100

    python version + cleanup

--- src/microsplit_reproducibility/configs/parameters/biosr.py
+++
@@ -1,8 +0,0 @@
-from ._base import SplittingParameters
-
-
-def get_musplit_parameters() -> dict:
-    return SplittingParameters().model_dump()
-
-def get_denoisplit_parameters() -> dict:
-    return SplittingParameters().model_dump()
